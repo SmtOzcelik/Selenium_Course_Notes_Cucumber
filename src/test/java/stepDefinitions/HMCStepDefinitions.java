e3ebe969c8e6ebe7051ef8209aa86fe1470ebe3f
samet ozcelik <smtab142790@gmail.com>
2023-05-09 17:09:00 +0300

    09/05/2023

--- src/test/java/stepDefinitions/HMCStepDefinitions.java
+++ src/test/java/stepDefinitions/HMCStepDefinitions.java
@@ -1,26 +1,34 @@
 package stepDefinitions;
 
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
+import pages.HMCPage;
+import utilities.ConfigReader;
 
 public class HMCStepDefinitions {
+    HMCPage hmcPage=new HMCPage();
     @Then("Log in yazisina tiklar")
     public void log_in_yazisina_tiklar() {
+        hmcPage.loginButonu.click();
 
     }
     @Then("gecerli username girer")
     public void gecerli_username_girer() {
-
+    hmcPage.usurnameKutusu.sendKeys(ConfigReader.getProperty("HMCValidUsername"));
     }
     @Then("gecerli password girer")
     public void gecerli_password_girer() {
+        hmcPage.passwordKutusu.sendKeys(ConfigReader.getProperty("HMCValidPassword"));
 
     }
     @Then("login butonuna basar")
     public void login_butonuna_basar() {
+        hmcPage.loginButonu2.click();
 
     }
     @Then("sayfaya giris yaptigini kontrol eder")
     public void sayfaya_giris_yaptigini_kontrol_eder() {
+        Assert.assertTrue(hmcPage.hotelManagement.isDisplayed());
 
     }
 
